7b5b33ff2c476224fbee8b5217f37f4328e25a8e
cqcode-1 <yiran865@gmail.com>
2020-06-10 07:57:57 +0800

    控制Tank移动方向

---
+++ src/com/john/tank/Main.java
@@ -0,0 +1,17 @@
+package com.john.tank;
+
+public class Main {
+    public static void main(String[] args) {
+        TankFrame tankFrame = new TankFrame();
+
+//        while (true){
+//            try {
+//                Thread.sleep(1000);
+//                tankFrame.repaint();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+
+    }
+}
--- src/com/john/tank/TankFrame.java
+++ src/com/john/tank/TankFrame.java
@@ -1,15 +1,23 @@
 package com.john.tank;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class TankFrame extends Frame {
+
+    int x = 200, y = 200;
+
     public TankFrame(){
         setSize(800, 600);
         setResizable(false);
         setTitle("TankFrame");
         setVisible(true);
+        //键盘监听事件
+        addKeyListener(new MyKeyListener());
+        //窗口关闭事件监听
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -20,6 +28,55 @@ public class TankFrame extends Frame {
 
     @Override
     public void paint(Graphics g) {
-        g.fillRect(200, 200, 50,50);
+        System.out.println("paint");
+        //位置和大小
+        g.fillRect(x, y, 50,50);
+//        x += 20;
+//        y += 20;
+    }
+
+    class MyKeyListener extends KeyAdapter{
+
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+            super.keyTyped(e);
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+            int keyCode = e.getKeyCode();
+            System.out.println("key pressed "+ keyCode);
+            switch (keyCode){
+                case KeyEvent.VK_LEFT:
+                    x -= 10;
+                    TankFrame.this.repaint();
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    x +=10;
+                    TankFrame.this.repaint();
+                    break;
+                case KeyEvent.VK_UP:
+                    y -=10;
+                    TankFrame.this.repaint();
+                    break;
+                case KeyEvent.VK_DOWN:
+                    y +=10;
+                    TankFrame.this.repaint();
+                    break;
+
+            }
+
+//            TankFrame.this.x += 20;
+//            TankFrame.this.y += 20;
+//            TankFrame.this.repaint();
+            super.keyPressed(e);
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            super.keyReleased(e);
+        }
     }
 }
