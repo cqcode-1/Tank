e8b58272e541f05e79e5738b00a8d19a067996f5
cqcode-1 <yiran865@gmail.com>
2020-06-10 06:48:59 +0800

    从frame类继承

--- src/com/john/tank/T.java
+++ src/com/john/tank/T.java
@@ -7,18 +7,6 @@ import java.awt.event.WindowListener;
 
 public class T {
     public static void main(String[] args) {
-        //窗口
-        Frame frame = new Frame();
-        frame.setSize(800, 600);
-        frame.setResizable(false);
-        frame.setTitle("Tank War");
-        //设置显示
-        frame.setVisible(true);
-        frame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent windowEvent) {
-                System.exit(0);
-            }
-        });
+        TankFrame tankFrame = new TankFrame();
     }
 }
